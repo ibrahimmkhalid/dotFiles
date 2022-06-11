4bd747207f48aacb6dcbd0084e433ab487f9f62a
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-06-11 03:24:31 -0700

    update with fonts installer

--- install.sh
+++ install.sh
@@ -23,3 +23,14 @@ ln -s $PWD/tmux.conf $HOME/.tmux.conf
 ln -s $PWD/tmux.conf.local $HOME/.tmux.conf.local
 ln -s $PWD/nvim $HOME/.config/nvim
 ln -s $PWD/scripts/tmux-session $HOME/.mybin/tmux-session
+
+echo "---------------------"
+echo "downloading firacode"
+mkdir tmp && cd tmp
+wget https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
+unzip Fira_Code_v6.2.zip
+cd ttf
+mkdir -p $HOME/tmp/.local/share/fonts
+mv *.ttf $HOME/tmp/.local/share/fonts/
+cd ../..
+rm -rf tmp
