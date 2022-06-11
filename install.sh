e6dc488e970080cf9781c18ce83f33be15d778f5
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-06-11 01:44:57 -0700

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 echo "---------------------"
 echo "Installing basic applications"
-sudo apt install make git wget curl tmux zsh ranger htop xsel xclip
+sudo add-apt-repository universe
+sudo apt install make git wget curl tmux zsh ranger htop xsel xclip libfuse2 -y
 
 echo "---------------------"
 echo "Cloning into submodules"
@@ -14,3 +15,11 @@ git config --global user.name "Ibrahim Mansoor Khalid"
 git config --global user.email "ibrahimmkhalid@gmail.com"
 git config --global core.editor nvim
 echo "done"
+
+echo "---------------------"
+echo "adding configs to appropriate locations"
+ln -s $PWD/zshrc $HOME/.zshrc
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/tmux.conf.local $HOME/.tmux.conf.local
+ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/scripts/tmux-session $HOME/.mybin/tmux-session
