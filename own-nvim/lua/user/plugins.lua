dfb0b3e538b78166be9e056cc74294c344f59d26
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-07-31 23:00:25 +0500

    update colorscheme to work with treesitter

--- own-nvim/lua/user/plugins.lua
+++ own-nvim/lua/user/plugins.lua
@@ -45,7 +45,8 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
-  use "morhetz/gruvbox" -- colorscheme
+--  use "morhetz/gruvbox" -- colorscheme
+  use "ellisonleao/gruvbox.nvim"
   
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
