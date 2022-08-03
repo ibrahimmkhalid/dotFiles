106994357ffd9711741e5637a80ed0082925305d
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-08-04 00:52:22 +0500

    color scheme

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -28,7 +28,7 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 
 
-local colorscheme = "tokyonight"
+local colorscheme = "gruvbox-material"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
