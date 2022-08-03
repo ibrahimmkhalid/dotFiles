ac4584785240d7bbfeee0bf15e5c2de31f4fa896
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-08-04 00:05:33 +0500

    update neotree config

--- nvim/lua/user/tree.lua
+++ nvim/lua/user/tree.lua
@@ -1,3 +1,4 @@
+
 local status_ok, tree = pcall(require, "neo-tree")
 if not status_ok then
   return
@@ -22,18 +23,48 @@ tree.setup {
   },
   window = {
     width = 50,
+    mapping_options = {
+      noremap = true,
+      nowait = true,
+    },
+    mappings = {
+      ["<space>"] = {
+          "toggle_node",
+          nowait = false,
+      },
+      ["Z"] = "expand_all_nodes",
+      ["m"] = {
+        "move",
+        config = {
+          show_path = "relative"
+        }
+      },
+      ["q"] = "close_window",
+      ["R"] = "refresh",
+      ["?"] = "show_help",
+      ["<"] = "prev_source",
+      [">"] = "next_source",
+    }
   },
   filesystem = {
     filtered_items = {
       visible = false,
       hide_dotfiles = true,
-      hide_gitignored = false,
+      hide_gitignored = true,
       hide_by_name = {
         ".DS_Store",
         "thumbs.db",
         "node_modules",
         "__pycache__",
       },
+    },
+    window = {
+      mappings = {
+        ["."] = "set_root",
+        ["H"] = "toggle_hidden",
+        ["<leader>gk"] = "prev_git_modified",
+        ["<leader>gj"] = "next_git_modified",
+      }
     },
     follow_current_file = true,
     hijack_netrw_behavior = "open_current",
