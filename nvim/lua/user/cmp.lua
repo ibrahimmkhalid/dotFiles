768d9d1965fff4ea176ae4883cf22969998cd11c
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-08-22 01:14:49 +0500

    fix some nvim keybinds

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -52,13 +52,13 @@ cmp.setup {
     end,
   },
   mapping = {
-    ["<S-k>"] = cmp.mapping.select_prev_item(),
-		["<S-j>"] = cmp.mapping.select_next_item(),
-    ["<S-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    ["<S-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-    ["<S-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<S-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<S-e>"] = cmp.mapping {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
@@ -81,7 +81,7 @@ cmp.setup {
       "i",
       "s",
     }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["<C-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
