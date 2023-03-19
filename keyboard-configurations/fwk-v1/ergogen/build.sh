2a14d7e42ea8318b1d49f4df2fcd387ec54719d2
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-03-18 21:30:21 -0700

    custom footprints for ergogen

--- keyboard-configurations/fwk-v1/ergogen/build.sh
+++ keyboard-configurations/fwk-v1/ergogen/build.sh
@@ -1,1 +1,15 @@
+cp ../../tools/ergogen/src/footprints/diode.js ../../tools/ergogen/src/footprints/diode.js.bak
+cp ./diode.js ../../tools/ergogen/src/footprints/diode.js
+
+cp ../../tools/ergogen/src/footprints/promicro.js ../../tools/ergogen/src/footprints/promicro.js.bak
+cp ./promicro.js ../../tools/ergogen/src/footprints/promicro.js
+
+cp ../../tools/ergogen/src/footprints/via.js ../../tools/ergogen/src/footprints/via.js.bak
+cp ./via.js ../../tools/ergogen/src/footprints/via.js
+
 ../../tools/ergogen/src/cli.js ./input.yaml -o output
+
+
+mv ../../tools/ergogen/src/footprints/diode.js.bak ../../tools/ergogen/src/footprints/diode.js
+mv ../../tools/ergogen/src/footprints/promicro.js.bak ../../tools/ergogen/src/footprints/promicro.js
+mv ../../tools/ergogen/src/footprints/via.js.bak ../../tools/ergogen/src/footprints/via.js
