f9ce12b21fe8adedac26b5769250ed5e701e09bf
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-03-25 17:22:05 -0700

    update

--- update-public-repo.sh
+++ update-public-repo.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
 # Define the source and destination directories
-src_dir="./"
-dest_dir="../dotFiles-public"
+src_dir="../dotFiles"
+dest_dir="../dotFiles-public-tmp"
 
 # Define the list of files and directories to exclude
-exclude_list=".git .gitsubmodules update-public-repo.sh ./keyboard-configurations/tools/ergogen ./keyboard-configurations/tools/qmk_firmware readme.md"
+exclude_list="./.git ./.gitmodules ./update-public-repo.sh ./keyboard-configurations/tools/ergogen ./keyboard-configurations/tools/qmk_firmware ./readme.md"
 
-# Copy the contents of the source directory to the destination directory, excluding the files and directories in the exclude list
-cp -r --parents $(find $src_dir -type f | grep -vE "$(echo $exclude_list | sed 's/ /|/g')") $dest_dir
+tmp_dir=$dest_dir.tmp
+cp $src_dir $tmp_dir -rf
+
+
+for item in $exclude_list; do
+  if [ "${item:0:1}" = "." ]; then
+    excluded_file_in_tmp_dir="$tmp_dir${item:1}"
+  else
+    excluded_file_in_tmp_dir="$tmp_dir/$item"
+  fi
+  rm -rf $excluded_file_in_tmp_dir
+done
+
+ls $tmp_dir -la
+cp -rf $tmp_dir/* $dest_dir/
+cp -rf $tmp_dir/.gitignore $dest_dir/
+
+
+rm -rf $tmp_dir
