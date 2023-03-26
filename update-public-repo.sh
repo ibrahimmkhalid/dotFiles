5d2a1e94dc6f8aa3e717687aaf575ed8424fe398
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-03-26 15:17:51 -0700

    final version of script for updating the public repo

--- update-public-repo.sh
+++ update-public-repo.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 src_dir="../dotFiles"
-dest_dir="../dotFiles-public-tmp"
+dest_dir="../dotFiles-public"
+tmp_dir="../dotFiles-tmp"
 
-exclude_list="./exclude.txt"
-echo './.git' >> $exclude_list
-echo './.gitmodules' >> $exclude_list
-echo './update-public-repo.sh' >> $exclude_list
-echo './keyboard-configurations/tools/ergogen' >> $exclude_list
-echo './keyboard-configurations/tools/qmk_firmware' >> $exclude_list
-echo './readme.md' >> $exclude_list
+cp -rf $src_dir $tmp_dir
+cd $tmp_dir
+rm './.git' -rf
+rm './.gitmodules' -rf
+rm './update-public-repo.sh' -rf
+rm './keyboard-configurations/tools/ergogen' -rf
+rm './keyboard-configurations/tools/qmk_firmware' -rf
+rm './readme.md' -rf
 
-echo '!./**/.git' >> $exclude_list
-echo '!./**/.gitmodules' >> $exclude_list
-echo '!./**/update-public-repo.sh' >> $exclude_list
-echo '!./**/keyboard-configurations/tools/ergogen' >> $exclude_list
-echo '!./**/keyboard-configurations/tools/qmk_firmware' >> $exclude_list
-echo '!./**/readme.md' >> $exclude_list
+cd $dest_dir
 
-rsync -av --delete --exclude-from=$exclude_list $src_dir/ $dest_dir/
-rm $exclude_list
+mv './.git' $tmp_dir
+mv './.gitmodules' $tmp_dir
+mv './keyboard-configurations/tools/ergogen' $tmp_dir/keyboard-configurations/tools/ergogen
+mv './keyboard-configurations/tools/qmk_firmware' $tmp_dir/keyboard-configurations/tools/qmk_firmware
+mv './readme.md' $tmp_dir/readme.md
+
+cd $src_dir
+rm -rf $dest_dir
+mv $tmp_dir $dest_dir
