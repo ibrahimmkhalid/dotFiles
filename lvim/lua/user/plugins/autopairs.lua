f3ee52f8938bab555d3b8bace681320e03695de1
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-04-20 19:01:04 -0700

    update

--- lvim/lua/user/plugins/autopairs.lua
+++ lvim/lua/user/plugins/autopairs.lua
@@ -21,5 +21,11 @@ return {
         highlight_grey = "LineNr",
       },
     })
+
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local ok, cmp = pcall(require, "cmp")
+    if ok then
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+    end
   end
 }
