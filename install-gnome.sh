68cdf75648f3049dfe8a69df6d88ae0679acf36c
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-04-30 00:22:24 -0700

    update

--- install-gnome.sh
+++ install-gnome.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
 
-#gui - install
+#install
 sudo apt install dconf-editor numix-icon-theme-circle alacritty gnome-tweaks -y
 
-#gui - setups
+#setups
 mkdir -p $HOME/.local/share/gnome-shell/extensions
 mkdir -p $HOME/.local/share/fonts
 mkdir -p $HOME/.config/alacritty
 
-#gui - configs
+#configs
 ln -s -f $PWD/my-assets $HOME/.local/share/my-assets
 sed "s|HOME_DIR|$HOME|g" guillotine.json.tmp > guillotine.json
 ln -s -f $PWD/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 ln -s -f $PWD/guillotine.json $HOME/.config/guillotine.json
 
-#gui - fonts
+#fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip
 rm *Windows*
@@ -24,7 +24,7 @@ rm *.otf
 mv *.ttf $HOME/.local/share/fonts/
 cd ..
 
-#gui - nordic theme
+#nordic theme
 mkdir tmp && cd tmp
 wget https://github.com/EliverLara/Nordic/releases/latest/download/Nordic-Polar-v40.tar.xz
 wget https://github.com/EliverLara/Nordic/releases/latest/download/Nordic-Polar.tar.xz
@@ -35,14 +35,14 @@ sudo mv Nordic-Polar /usr/share/themes
 cd ..
 rm -rf tmp
 
-#gui - install extensions
+#install extensions
 sudo rm -rf /usr/share/gnome-shell/extensions/*
 wget -N -q "https://raw.githubusercontent.com/cyfrost/install-gnome-extensions/master/install-gnome-extensions.sh" -O ./extensions/install-gnome-extensions.sh
 chmod +x extensions/install-gnome-extensions.sh 
 ./extensions/install-gnome-extensions.sh --enable --file extensions/links.txt
 rm -f ./extensions/install-gnome-extensions.sh 
 
-#gui - gnome settings
+#gnome settings
 dconf load /org/gnome/ < org-gnome.dconf.dump
 gsettings set org.gnome.desktop.background picture-uri file:////$PWD/wallpaper/wallpaper.png
 gsettings set org.gnome.desktop.screensaver picture-uri file:////$PWD/wallpaper/wallpaper.png
--- install-shell.sh
+++ install-shell.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-#cli - install
+#install
 sudo add-apt-repository universe -y
 sudo add-apt-repository ppa:aslatter/ppa -y
 sudo apt update
 sudo apt install make gawk wget curl tmux zsh ranger htop libfuse2 ripgrep gcc g++ unzip neofetch tldr lldb entr fzf xsel xclip -y
 
-#cli - setups
+#setups
 git config --global user.name "Ibrahim Mansoor Khalid"
 git config --global user.email "ibrahimmkhalid@gmail.com"
 git config --global core.editor nvim
@@ -15,19 +15,19 @@ mkdir -p $HOME/.local/bin
 mkdir -p $HOME/.local/lib
 mkdir -p $HOME/.local/share
 
-#cli - tmux plugin manager
+#tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
-#cli - node version manager
+#node version manager
 nvm_version=$(curl -s https://raw.githubusercontent.com/nvm-sh/nvm/master/README.md | grep -o 'nvm/v[0-9]*\.[0-9]*\.[0-9]*' | grep -o 'v[0-9]*\.[0-9]*\.[0-9]*' | head -1)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$nvm_version/install.sh | bash
 
-#cli - neovim
+#neovim
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
 mv $PWD/nvim.appimage $HOME/.local/bin/nvim
 
-#cli - lazy git
+#lazy git
 mkdir tmp && cd tmp
 curl -LO https://github.com/jesseduffield/lazygit/releases/download/v0.37/lazygit_0.37_Linux_x86_64.tar.gz
 tar xzf lazygit_0.37_Linux_x86_64.tar.gz
@@ -35,10 +35,10 @@ mv $PWD/lazygit $HOME/.local/bin/lazygit
 cd ..
 rm -rf tmp
 
-#cli - zsh theme, ohmyzsh
+#zsh theme, ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#cli - configs
+#configs
 ln -s -f $PWD/zshrc $HOME/.zshrc
 ln -s -f $PWD/tmux $HOME/.config/tmux
 ln -s -f $PWD/nvim $HOME/.config/nvim
