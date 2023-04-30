24873ee6bc2001d7bfa515feee1b860412f5f284
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-04-29 17:31:18 -0700

    better nvim nav

--- nvim/lua/user/keybinds.lua
+++ nvim/lua/user/keybinds.lua
@@ -17,8 +17,10 @@ Keymap("n", "<C-Left>", ":vertical resize -2<CR>", "Increase window verticaly")
 Keymap("n", "<C-Right>", ":vertical resize +2<CR>", "Decrease window verticaly")
 
 -- fast scrolling
-Keymap("n", "<S-j>", "5j", "5j")
-Keymap("n", "<S-k>", "5k", "5k")
+Keymap("n", "<C-I>", "<C-U>zz", "Scroll up")
+Keymap("n", "<C-U>", "<C-D>zz", "Scroll down")
+Keymap("n", "n", "nzzzv", "Next highlight")
+Keymap("n", "N", "Nzzzv", "Previous highlight")
 
 -- saving files and exiting nvim
 Keymap("n", "<leader>w", ":w<CR>", "save")
@@ -42,16 +44,12 @@ Keymap("v", "<", "<gv", "de-indent")
 Keymap("v", ">", ">gv", "indent")
 
 -- Move text up and down
-Keymap("v", "J", "5j", "5j")
-Keymap("v", "K", "5k", "5k")
 Keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", "move selected text down")
 Keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", "move selected text up")
 Keymap("v", "p", '"_dP', "visual paste")
 
 -- Visual Block --
 -- Move text up and down
-Keymap("x", "J", "5j", "5j")
-Keymap("x", "K", "5k", "5k")
 Keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", "move selected text down")
 Keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", "move selected text up")
 
@@ -61,3 +59,5 @@ vim.api.nvim_set_keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", { silent = true, desc
 vim.api.nvim_set_keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", { silent = true, desc = "Focus bottom window" })
 vim.api.nvim_set_keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", { silent = true, desc = "Focus upper window" })
 vim.api.nvim_set_keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", { silent = true, desc = "Focus right window" })
+vim.api.nvim_set_keymap("n", "<S-K>", "", {silent = true, noremap = true,}) -- removing bad habit
+vim.api.nvim_set_keymap("n", "<S-J>", "", {silent = true, noremap = true,})
