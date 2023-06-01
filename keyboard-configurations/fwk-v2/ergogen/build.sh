ae96ee245b8e428cb29184e6d5a71c6e07507c8d
Ibrahim Mansoor Khalid <32709895+ibrahimmkhalid@users.noreply.github.com>
2023-05-31 20:43:09 -0700

    new keyboard versions (#23) | Just work in the main branch lol???
    * make seperate desgins for gamer and worker layouts
    
    * update ergogen src
    
    * update

--- keyboard-configurations/fwk-v2/ergogen/build.sh
+++ keyboard-configurations/fwk-v2/ergogen/build.sh
@@ -7,7 +7,8 @@ cp ./promicro.js ../../tools/ergogen/src/footprints/promicro.js
 cp ../../tools/ergogen/src/footprints/via.js ../../tools/ergogen/src/footprints/via.js.bak
 cp ./via.js ../../tools/ergogen/src/footprints/via.js
 
-../../tools/ergogen/src/cli.js ./input.yaml -o output
+../../tools/ergogen/src/cli.js ./input-gamer.yaml -o output-gamer
+../../tools/ergogen/src/cli.js ./input-worker.yaml -o output-worker
 
 
 mv ../../tools/ergogen/src/footprints/diode.js.bak ../../tools/ergogen/src/footprints/diode.js
