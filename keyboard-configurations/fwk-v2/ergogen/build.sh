9cc4ae956d1d8afa0af0ab9954dae81a310e911a
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-06-05 01:33:53 -0700

    update fwk2

--- keyboard-configurations/fwk-v2/ergogen/build.sh
+++ keyboard-configurations/fwk-v2/ergogen/build.sh
@@ -7,9 +7,7 @@ cp ./promicro.js ../../tools/ergogen/src/footprints/promicro.js
 cp ../../tools/ergogen/src/footprints/via.js ../../tools/ergogen/src/footprints/via.js.bak
 cp ./via.js ../../tools/ergogen/src/footprints/via.js
 
-../../tools/ergogen/src/cli.js ./input-gamer.yaml -o output-gamer
-../../tools/ergogen/src/cli.js ./input-worker.yaml -o output-worker
-
+../../tools/ergogen/src/cli.js ./input.yaml -o output
 
 mv ../../tools/ergogen/src/footprints/diode.js.bak ../../tools/ergogen/src/footprints/diode.js
 mv ../../tools/ergogen/src/footprints/promicro.js.bak ../../tools/ergogen/src/footprints/promicro.js
