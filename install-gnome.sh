f2076b76970e7eed1e81313bace11dbc0fcc3fbf
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-07-03 19:06:00 -0700

    remove icon madness

--- install-gnome.sh
+++ install-gnome.sh
@@ -10,7 +10,6 @@ mkdir -p $HOME/.local/share/fonts
 mkdir -p $HOME/.config/alacritty
 
 #configs
-ln -s -f $PWD/my-assets $HOME/.local/share/my-assets
 sed "s|HOME_DIR|$HOME|g" guillotine.json.tmp > guillotine.json
 ln -s -f $PWD/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 ln -s -f $PWD/guillotine.json $HOME/.config/guillotine.json
