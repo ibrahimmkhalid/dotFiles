762ecb022e3c7fc2eac65348a71d40e4bcfcd423
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-08-15 00:36:25 -0700

    organize better

--- nvim/lua/user/plugins/basic-plugins.lua
+++ nvim/lua/user/plugins/basic-plugins.lua
@@ -3,21 +3,21 @@ return {
   "nvim-lua/plenary.nvim",
   "nvim-tree/nvim-web-devicons",
   {
-    "sainnhe/everforest",
-    lazy = false,
-    priority = 1000,
-    dependencies = {
-      "ellisonleao/gruvbox.nvim",
-      "folke/tokyonight.nvim",
-      "sainnhe/gruvbox-material",
-      "rebelot/kanagawa.nvim",
-    },
-    config = function()
-      local colorscheme = "everforest"
-      local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-      if not status_ok then
-        vim.notify("colorscheme " .. colorscheme .. " not found!")
+    "ellisonleao/gruvbox.nvim",
+    "folke/tokyonight.nvim",
+    "sainnhe/gruvbox-material",
+    "rebelot/kanagawa.nvim",
+    {
+      "sainnhe/everforest",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        local colorscheme = "everforest"
+        local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+        if not status_ok then
+          vim.notify("colorscheme " .. colorscheme .. " not found!")
+        end
       end
-    end
+    }
   },
 }
