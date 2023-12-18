0f253b3fd28f25a72de79a6b61bfae7a959c891d
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-12-18 02:42:36 -0800

    nvim: treesitter playground
    just for fun

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -3,13 +3,14 @@ return {
   dependencies = {
     "HiPhish/rainbow-delimiters.nvim",
     "JoosepAlviste/nvim-ts-context-commentstring",
+    "nvim-treesitter/playground",
   },
   build = function()
     pcall(vim.cmd, 'TSUpdate')
   end,
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = "python",
+      ensure_installed = { "python", "lua" },
       sync_install = false,
       ignore_install = { "" }, -- List of parsers to ignore installing
     })
