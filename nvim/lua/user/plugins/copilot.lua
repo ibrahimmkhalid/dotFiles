895798a6127d8caff6fbb48cf1d047a0a052b897
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-12-19 20:02:17 -0800

    nvim: global persistent copilot toggle

--- nvim/lua/user/plugins/copilot.lua
+++ nvim/lua/user/plugins/copilot.lua
@@ -14,11 +14,48 @@ return {
       xml = false,
       json = false,
     }
+
+    local check_copilot_global_status = function ()
+      local status_file = "~/.local/share/nvim/copilot_status"
+      vim.cmd("silent !touch " .. status_file)
+      local status = vim.fn.system("cat " .. status_file)
+      if status == "1\n" then
+        return true
+      else
+        return false
+      end
+    end
+
+    _G.toggle_copilot_global_status = function ()
+      local status = check_copilot_global_status()
+      local status_file = "~/.local/share/nvim/copilot_status"
+      if status then
+        vim.cmd("silent !echo 0 > " .. status_file)
+        vim.cmd("Copilot disable")
+      else
+        vim.cmd("silent !echo 1 > " .. status_file)
+        vim.cmd("Copilot enable")
+      end
+    end
+
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function ()
+        local status = check_copilot_global_status()
+        if status then
+          vim.g.copilot_enabled = true
+        else
+          vim.g.copilot_enabled = false
+        end
+      end
+    })
+
     vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("\\<CR>")', { replace_keycodes = false, expr = true, noremap = false })
     vim.api.nvim_set_keymap("i", "<C-l>", '<Plug>(copilot-accept-word)', { noremap = false })
     vim.api.nvim_set_keymap("i", "<C-j>", '<Plug>(copilot-next)', { noremap = false })
     vim.api.nvim_set_keymap("i", "<C-k>", '<Plug>(copilot-previous)', { noremap = false })
     vim.api.nvim_set_keymap("i", "<M-/>", '<Plug>(copilot-suggest)', { noremap = false })
     vim.api.nvim_set_keymap("i", "<C-e>", '<Plug>(copilot-dismiss)', { noremap = false })
+    vim.api.nvim_set_keymap("n", "<leader>agi", ":lua toggle_copilot_global_status()<CR>", { noremap = true, silent = true, desc = "Toggle Artificial Github Intelligence" })
   end
 }
