a9f9f7faa17867534bb412f6d4ff9617051b39d3
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-12-27 22:21:41 -0800

    feat(ansible): add qmk to ansible script

--- install-shell.sh
+++ install-shell.sh
@@ -7,9 +7,3 @@ sudo add-apt-repository ppa:aslatter/ppa -y
 sudo apt update
 sudo apt install make gawk wget curl ranger htop libfuse2 ripgrep gcc g++ unzip neofetch tldr lldb entr fzf xsel xclip python3 python-is-python3 python3-pip build-essential python3-venv ansible -y
 
-# qmk
-git clone https://github.com/qmk/qmk_firmware ~/.local/share/qmk_firmware
-python3 -m pip install --user qmk
-
-PATH=$PATH:$HOME/.local/bin
-qmk setup -H ~/.local/share/qmk_firmware -y
