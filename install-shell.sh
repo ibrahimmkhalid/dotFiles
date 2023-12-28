46ebbb7103dae12ec765ec18a0367aa50d834d7d
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-12-27 23:43:12 -0800

    feat(ansible): convert all of install-shell.sh to ansible

--- install-shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-#install
-sudo add-apt-repository universe -y
-sudo add-apt-repository ppa:aslatter/ppa -y
-sudo apt update
-sudo apt install make gawk wget curl ranger htop libfuse2 ripgrep gcc g++ unzip neofetch tldr lldb entr fzf xsel xclip python3 python-is-python3 python3-pip build-essential python3-venv ansible -y
-
