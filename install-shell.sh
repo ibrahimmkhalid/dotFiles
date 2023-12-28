86db2ae912418d9ef9923afe9fbd6d57946f9783
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-12-27 20:05:23 -0800

    feat(ansible): add ergogen to ansible script

--- install-shell.sh
+++ install-shell.sh
@@ -7,17 +7,9 @@ sudo add-apt-repository ppa:aslatter/ppa -y
 sudo apt update
 sudo apt install make gawk wget curl ranger htop libfuse2 ripgrep gcc g++ unzip neofetch tldr lldb entr fzf xsel xclip python3 python-is-python3 python3-pip build-essential python3-venv ansible -y
 
-#ergogen and qmk
-git clone https://github.com/ergogen/ergogen ~/.local/share/ergogen
+# qmk
 git clone https://github.com/qmk/qmk_firmware ~/.local/share/qmk_firmware
 python3 -m pip install --user qmk
 
-# curl -L https://bit.ly/n-install | N_PREFIX=~/.local/share/n bash -s -- -y
-# export N_PREFIX="$HOME/.local/share/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
-
-#final installs
-cd ~/.local/share/ergogen
-npm install
 PATH=$PATH:$HOME/.local/bin
 qmk setup -H ~/.local/share/qmk_firmware -y
-ln -s -f ~/.local/share/ergogen/src/cli.js ~/.local/bin/ergogen
