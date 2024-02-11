cb8e4325648c84aa49da98c504efbeb5914a3ca3
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-02-10 16:32:07 -0800

    wsl compatibility for vimtex

--- nvim/lua/user/plugins/latex.lua
+++ nvim/lua/user/plugins/latex.lua
@@ -1,6 +1,13 @@
 return {
   'lervag/vimtex',
   config = function()
-    vim.g.vimtex_view_method = 'zathura'
+    -- check if running in WSL by checking if powershell.exe is found with 'which'
+    local is_wsl = vim.fn.system('which powershell.exe') ~= ''
+    if is_wsl then
+      vim.g.vimtex_view_general_viewer = '/home/ibrahim/.local/bin/SumatraPDF_synctex.sh'
+      vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+    else
+      vim.g.vimtex_view_method = 'zathura'
+    end
   end
 }
