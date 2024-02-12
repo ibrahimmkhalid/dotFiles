39424fe275cfff2fdf0dffca7bdeae2a9173ac62
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-02-11 22:17:13 -0800

    use better wsl detection

--- nvim/lua/user/plugins/latex.lua
+++ nvim/lua/user/plugins/latex.lua
@@ -1,8 +1,7 @@
 return {
   'lervag/vimtex',
   config = function()
-    -- check if running in WSL by checking if powershell.exe is found with 'which'
-    local is_wsl = vim.fn.system('which powershell.exe') ~= ''
+    local is_wsl = vim.fn.system('uname -r | grep -i "microsoft"') ~= ''
     if is_wsl then
       vim.g.vimtex_view_general_viewer = '/home/ibrahim/.local/bin/SumatraPDF_synctex.sh'
       vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
