702b5a4cc89e2c54ad81f43c09d8d65f95effa8d
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-03-03 18:57:29 -0800

    update jupyter keybinds

--- nvim/lua/user/plugins/jupyter.lua
+++ nvim/lua/user/plugins/jupyter.lua
@@ -7,7 +7,7 @@ return {
     vim.api.nvim_set_keymap("n", "<leader>jx", "<Cmd>w<CR><Cmd>call jupyter_ascending#execute()<CR>", { silent = true, noremap = true, desc = "Run current jupyter cell"})
     vim.api.nvim_set_keymap("n", "<leader>ja", "<Cmd>w<CR><Cmd>call jupyter_ascending#execute_all()<CR>", { silent = true, noremap = true, desc = "Run jupyter notebook"})
     vim.api.nvim_set_keymap("n", "<leader>jr", "<Cmd>w<CR><Cmd>call jupyter_ascending#restart()<CR>", { silent = true, noremap = true, desc = "Restart Jupyter notebook"})
-    vim.api.nvim_set_keymap("n", "<leader>jc", "<Cmd>w<CR>o<CR># %%<CR>", { silent = true, noremap = true, desc = "New code block"})
-    vim.api.nvim_set_keymap("n", "<leader>jm", "<Cmd>w<CR>o<CR># %% [markdown]<CR># ", { silent = true, noremap = true, desc = "New markdown block"})
+    vim.api.nvim_set_keymap("n", "<leader>jc", "<Cmd>w<CR>o<CR><Esc>0i# %%<CR>", { silent = true, noremap = true, desc = "New code block"})
+    vim.api.nvim_set_keymap("n", "<leader>jm", "<Cmd>w<CR>o<CR><Esc>0i# %% [markdown]<CR># ", { silent = true, noremap = true, desc = "New markdown block"})
   end
 }
