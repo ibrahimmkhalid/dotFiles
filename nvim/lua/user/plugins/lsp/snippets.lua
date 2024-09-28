972b2254aa64248f68c7dd255ff3de24c3c0a502
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-09-28 00:31:01 -0700

    nvim:snippets: autoformat

--- nvim/lua/user/plugins/lsp/snippets.lua
+++ nvim/lua/user/plugins/lsp/snippets.lua
@@ -3,39 +3,39 @@ local ls = require("luasnip")
 local s = ls.s
 local t = ls.text_node
 
-ls.add_snippets( "lua", {
-    s({name="DAP Configuration for Python", trig="dappython"}, { t({
-      "local dap = require(\"dap\")",
-      "dap.adapters.python = {",
-      "  type = \"executable\",",
-      "  command = \"debugpy-adapter\",",
-      "  args = {},",
-      "}",
-      "dap.configurations.python = {",
-      "  {",
-      "    type = \"python\",",
-      "    request = \"launch\",",
-      "    name = \"Launch file\",",
-      "    program = \"${file}\",",
-      "    pythonPath = \"python\"",
-      "  }",
-      "}",
-    })})
-  }
+ls.add_snippets("lua", {
+  s({ name = "DAP Configuration for Python", trig = "dappython" }, { t({
+    "local dap = require(\"dap\")",
+    "dap.adapters.python = {",
+    "  type = \"executable\",",
+    "  command = \"debugpy-adapter\",",
+    "  args = {},",
+    "}",
+    "dap.configurations.python = {",
+    "  {",
+    "    type = \"python\",",
+    "    request = \"launch\",",
+    "    name = \"Launch file\",",
+    "    program = \"${file}\",",
+    "    pythonPath = \"python\"",
+    "  }",
+    "}",
+  }) })
+}
 )
 
-ls.add_snippets( "all", {
-    s({name="Basic project environment for python", trig="pyproj"}, { t({
-      "source ~/pyenv_sjsu/bin/activate",
-    })}),
-    s({name="Conda project environment for python", trig="condaproj"}, { t({
-      "source ~/conda_env/bin/activate",
-    })}),
-    s({name="Basic project environment for python on wayland", trig="pyprojgui"}, { t({
-      "source ~/pyenv_sjsu/bin/activate",
-      "export QT_QPA_PLATFORM=wayland",
-    })})
-  }
+ls.add_snippets("all", {
+  s({ name = "Basic project environment for python", trig = "pyproj" }, { t({
+    "source ~/pyenv_sjsu/bin/activate",
+  }) }),
+  s({ name = "Conda project environment for python", trig = "condaproj" }, { t({
+    "source ~/conda_env/bin/activate",
+  }) }),
+  s({ name = "Basic project environment for python on wayland", trig = "pyprojgui" }, { t({
+    "source ~/pyenv_sjsu/bin/activate",
+    "export QT_QPA_PLATFORM=wayland",
+  }) })
+}
 )
 
 ls.add_snippets("python", {
