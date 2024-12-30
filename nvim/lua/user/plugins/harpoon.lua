51b133cdb13b2740dcdf07d044b143ac4470e5f0
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-12-29 22:16:47 -0800

    reformat

--- nvim/lua/user/plugins/harpoon.lua
+++ nvim/lua/user/plugins/harpoon.lua
@@ -10,36 +10,84 @@ return {
       }
     })
 
-    vim.api.nvim_set_keymap("n", "<leader>mm", "", { silent = true, noremap = true, desc = "Open harpoon list", callback = function ()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end})
+    vim.api.nvim_set_keymap("n", "<leader>mm", "",
+      {
+        silent = true,
+        noremap = true,
+        desc = "Open harpoon list",
+        callback = function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end
+      })
 
-    vim.api.nvim_set_keymap("n", "<leader>ma", "", { silent = true, noremap = true, desc = "Add to harpoon list", callback = function ()
-      harpoon:list():add()
-    end})
+    vim.api.nvim_set_keymap("n", "<leader>ma", "",
+      {
+        silent = true,
+        noremap = true,
+        desc = "Add to harpoon list",
+        callback = function()
+          harpoon:list():add()
+        end
+      })
 
-    vim.api.nvim_set_keymap("n", "<M-r>", "", { silent = true, noremap = true, desc = "Harpoon item 1", callback = function ()
-      harpoon:list():select(1)
-    end})
+    vim.api.nvim_set_keymap("n", "<M-r>", "",
+      {
+        silent = true,
+        noremap = true,
+        desc = "Harpoon item 1",
+        callback = function()
+          harpoon:list():select(1)
+        end
+      })
 
-    vim.api.nvim_set_keymap("n", "<M-e>", "", { silent = true, noremap = true, desc = "Harpoon item 2", callback = function ()
-      harpoon:list():select(2)
-    end})
+    vim.api.nvim_set_keymap("n", "<M-e>", "",
+      {
+        silent = true,
+        noremap = true,
+        desc = "Harpoon item 2",
+        callback = function()
+          harpoon:list():select(2)
+        end
+      })
 
-    vim.api.nvim_set_keymap("n", "<M-w>", "", { silent = true, noremap = true, desc = "Harpoon item 3", callback = function ()
-      harpoon:list():select(3)
-    end})
+    vim.api.nvim_set_keymap("n", "<M-w>", "",
+      {
+        silent = true,
+        noremap = true,
+        desc = "Harpoon item 3",
+        callback = function()
+          harpoon:list():select(3)
+        end
+      })
 
-    vim.api.nvim_set_keymap("n", "<M-q>", "", { silent = true, noremap = true, desc = "Harpoon item 4", callback = function ()
-      harpoon:list():select(4)
-    end})
+    vim.api.nvim_set_keymap("n", "<M-q>", "",
+      {
+        silent = true,
+        noremap = true,
+        desc = "Harpoon item 4",
+        callback = function()
+          harpoon:list():select(4)
+        end
+      })
 
-    vim.api.nvim_set_keymap("n", "<M-n>", "", { silent = true, noremap = true, desc = "Next harpoon item", callback = function ()
-      harpoon:list():next()
-    end})
+    vim.api.nvim_set_keymap("n", "<M-n>", "",
+      {
+        silent = true,
+        noremap = true,
+        desc = "Next harpoon item",
+        callback = function()
+          harpoon:list():next()
+        end
+      })
 
-    vim.api.nvim_set_keymap("n", "<M-p>", "", { silent = true, noremap = true, desc = "Previous harpoon item", callback = function ()
-      harpoon:list():prev()
-    end})
+    vim.api.nvim_set_keymap("n", "<M-p>", "",
+      {
+        silent = true,
+        noremap = true,
+        desc = "Previous harpoon item",
+        callback = function()
+          harpoon:list():prev()
+        end
+      })
   end
 }
